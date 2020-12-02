38e926dad6ce9d74f00119a285b448a04fea86dd
unknown <denniswillie2000@gmail.com>
2020-12-02 13:03:34 +0000

    change heading in login

--- desktop-client/src/components/Login.js
+++ desktop-client/src/components/Login.js
@@ -3,11 +3,10 @@ import { GoogleLoginButton } from "react-social-login-buttons";
 export default function Login() {
   return <div id="loginPageContainer">
     <center><div id="watchDiaryIntro">
-      <h1 className="WatchDiaryTitle" style={{marginBottom: "0"}}>Watch Diary</h1>
+      <h1 className="WatchDiaryTitle" style={{marginBottom: "0"}}>Movelp</h1>
       <div className="WatchDiarySubTitleWrapper">
         <img src={process.env.PUBLIC_URL + '/images/clapperboard.png'} style={{width: "13em"}} alt="clapperboard"/>
-        <h3 className="WatchDiarySubTitle" style={{marginTop:"0"}}> Write your own watch diary. Let movies
-        have a special place in your heart.</h3>
+        <h3 className="WatchDiarySubTitle" style={{marginTop:"0"}}>All in one platform for all movie lovers.</h3>
       </div>
     </div></center>
     <center><div id="loginButton">
