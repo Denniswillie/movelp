b55ec9615cd74d3374ed5437258f48e91d128662
unknown <denniswillie2000@gmail.com>
2020-12-07 17:00:40 +0000

    change client credentials oauth google

--- desktop-client/src/components/Posts.js
+++ desktop-client/src/components/Posts.js
@@ -4,6 +4,10 @@ export default function Posts(props) {
   return <div>
     <h1>Posts</h1>
 
+    <form action="/createPost" method="post" enctype="multipart/form-data">
+      <input type="file" name="image" />
+      <input type="text" name="content" />
+    </form>
     <button
       variant="contained"
       color="primary"
