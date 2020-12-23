f42a4a5e57730b720a016d6552236df7a69699d7
unknown <denniswillie2000@gmail.com>
2020-12-23 17:04:01 +0000

    save diary post template

--- desktop-client/src/components/display/DiaryBox.js
+++ desktop-client/src/components/display/DiaryBox.js
@@ -3,6 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
+import Button from '@material-ui/core/Button';
+import MovieIcon from '@material-ui/icons/Movie';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import CommentIcon from '@material-ui/icons/Comment';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,6 +16,11 @@ const useStyles = makeStyles((theme) => ({
     '& > *': {
       margin: theme.spacing(1),
     },
+  },
+  button: {
+    margin: theme.spacing(1),
+    position: "absolute",
+    marginLeft: "421px"
   },
 }));
 
@@ -41,9 +52,14 @@ export default function DiaryBox(props) {
       boxShadow: "0 0 2px #999"}}>
     <div>
       <div style={{width: "100%", margin: "auto", textAlign: "left", padding: "5px", display: "flex"}}>
-      <div className={classes.root}>
-        <Avatar src={process.env.PUBLIC_URL + '/images/loginImage.png'} />
-      </div>
+        <Button
+          style={{backgroundColor: "black", color: "white"}}
+          variant="contained"
+          className={classes.button}
+          startIcon={<MovieIcon />}>Related movies</Button>
+        <div className={classes.root}>
+          <Avatar src={process.env.PUBLIC_URL + '/images/loginImage.png'} />
+        </div>
         <p style={{bottom: "0", fontFamily: "roboto", fontWeight: "700"}}>Dennis Willie</p>
       </div>
     </div>
@@ -51,6 +67,29 @@ export default function DiaryBox(props) {
       This is diary post. This type of post consists of the writer's experience with the movie. It can be a sad experience,
       happy, or whatever kinds of experience that the user may have.
     </div>
-    <ImageGallery items={images} />
+    <ImageGallery items={images} showFullscreenButton={false} showPlayButton={false} showThumbnails={false}/>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      flexWrap: "wrap",
+      padding: "10px"
+    }}>
+      <ThumbUpAltIcon />
+      <span style={{fontFamily: "roboto", marginLeft: "6px"}}>65</span>
+      <CommentIcon style={{marginLeft: "20px"}}/>
+      <span style={{fontFamily: "roboto", marginLeft: "6px"}}>65</span>
+    </div>
+    <div style={{padding: "5px"}}>
+    <div style={{borderTop: "1px solid #9ba89e", paddingTop: "5px"}}>
+      <Button style={{width: "30%"}} name="createDiaryButton" onClick={props.handleClick}>
+        <ThumbUpAltOutlinedIcon />
+        <p style={{fontFamily: 'Roboto', marginLeft: "6px"}} id="createDiaryButton">Like</p>
+      </Button>
+      <Button style={{width: "30%"}} name="createRecommendationButton" onClick={props.handleClick}>
+        <ChatBubbleOutlineOutlinedIcon />
+        <p style={{fontFamily: 'Roboto', marginLeft: "6px"}} id="createRecommendationButton">Comment</p>
+      </Button>
+    </div>
+    </div>
   </div>
 }
