9f19e014225d9aab3731d5f03ad66664db5f091a
unknown <denniswillie2000@gmail.com>
2021-01-04 21:09:51 +0000

    sfese

--- desktop-client/src/components/Login.js
+++ desktop-client/src/components/Login.js
@@ -1,23 +1,27 @@
 import { GoogleLoginButton } from "react-social-login-buttons";
+import {
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 
 export default function Login() {
   function handleClick() {
     window.location.href = "/auth/google";
   }
   return <div id="loginPageContainer">
-    <center><div id="watchDiaryIntro">
+    <div id="watchDiaryIntro">
       <h1 className="WatchDiaryTitle" style={{marginBottom: "0"}}>Movelp</h1>
       <div className="WatchDiarySubTitleWrapper">
         <img src={process.env.PUBLIC_URL + '/images/clapperboard.png'} style={{width: "13em"}} alt="clapperboard"/>
-        <h3 className="WatchDiarySubTitle" style={{marginTop:"0"}}>All in one platform for all movie lovers.</h3>
+        <h3 className="WatchDiarySubTitle" style={{marginTop:"0"}}>Made for all movie lovers.</h3>
       </div>
-    </div></center>
-    <center><div id="loginButton">
+    </div>
+    <div style={{width: isBrowser ? "30%" : "90%", margin: "auto"}}>
       <GoogleLoginButton
         onClick={handleClick}
-        style={{backgroundColor: "black", fontFamily: "Nerko One"}}
+        style={{backgroundColor: "black", fontFamily: "Nerko One", margin: "auto"}}
         activeStyle={{backgroundColor: "#092e6b"}}
       />
-    </div></center>
+    </div>
   </div>
 }
